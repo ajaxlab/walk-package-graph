e3c9c0810d120e693509a6a09025d1edd7e3724c
Shim Heungwoon <ajaxlab7@gmail.com>
2019-03-11 16:16:02 +0900

    [env] setup development environment

---
+++ src/walkPackageGraph.ts
@@ -0,0 +1,5 @@
+function walkPackageGraph() {
+
+}
+
+export default walkPackageGraph;
---
+++ test/walkPackageGraph.spec.ts
@@ -0,0 +1,10 @@
+import { expect, use } from 'chai';
+import walkPackageGraph from '../src/walkPackageGraph';
+
+describe('walkPackageGraph', function () {
+
+  it('todo', function () {
+    // todo
+  });
+
+});
