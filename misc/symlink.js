c692dbb2b424a05b5a0cb1b6d4899b7d22f02855
Shim Heungwoon <ajaxlab7@gmail.com>
2019-03-22 11:39:27 +0900

    [misc] remove unused test files

--- misc/symlink.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-fs.symlink('./real', './sym', 'dir', (e) => {
-  console.info(e);
-});
--- misc/tick2.js
+++
@@ -1,9 +0,0 @@
-process.nextTick(() => {
-  console.log('tick');
-});
-
-setTimeout(() => {
-  console.log('timeout');
-}, 1000);
-
-console.log('now');
